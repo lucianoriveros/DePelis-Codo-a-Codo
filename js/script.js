f6baee67812c29891ee3f02baab65207e5b2efc8
Orlando Perez <OrlandoPerez>
2023-10-01 13:46:18 -0300

    header

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// SCRIPT DEL HEADER
+const menu = document.querySelector('i');
+const links = document.querySelector('nav');
+
+menu.addEventListener("click", () =>{
+    links.classList.toggle("mobile-menu")
+});
+// FIN DEL HEADER
